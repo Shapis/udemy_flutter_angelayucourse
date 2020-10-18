61306e4865ca7fcde2f640d01e36b4a1bfd9c876
Henrique Silva <hpsilva+git@pm.me>
2020-10-18 20:44:05 -0300

    Finished Lesson9

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -34,6 +35,45 @@ class MyApp extends StatelessWidget {
                   fontSize: 20.0,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 20.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                margin: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+1 323 599 0056',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'ashleyttolentino@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                    ),
+                  ),
                 ),
               ),
             ],
