3b8221aab1dddda43507b07fd4c5ff7c337cd866
Henrique Silva <hpsilva+git@pm.me>
2020-10-17 22:43:55 -0300

    Finished Lesson2

--- tutorial1/lib/main.dart
+++ tutorial1/lib/main.dart
@@ -1,10 +1,21 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://i.imgur.com/w8vW4Sx.jpeg'),
+          ),
+        ),
       ),
     ),
   );
---
+++ tutorial2/android/app/src/main/kotlin/hpsilva/com/tutorial2/MainActivity.kt
@@ -0,0 +1,6 @@
+package hpsilva.com.tutorial2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
