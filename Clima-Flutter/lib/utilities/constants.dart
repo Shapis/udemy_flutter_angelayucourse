654195dff28c2b2ddd394f8f549140a85903067c
Henrique Silva <hpsilva+git@pm.me>
2020-10-30 07:18:16 -0300

    Finished Section13

--- Clima-Flutter/lib/utilities/constants.dart
+++ Clima-Flutter/lib/utilities/constants.dart
@@ -20,3 +20,22 @@ const kConditionTextStyle = TextStyle(
 );
 
 const String kAPIKey = '78cc588ef511f9364dd83474ef166495';
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white,
+  icon: Icon(
+    Icons.location_city,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0),
+    ),
+    borderSide: BorderSide.none,
+  ),
+);
