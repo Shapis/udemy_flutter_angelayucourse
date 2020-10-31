a05af956d241685dd5944898b41a3d2dd3b02219
Henrique Silva <hpsilva+git@pm.me>
2020-10-31 06:10:53 -0300

    Halfway through Section14

--- bitcoin-ticker-flutter/lib/price_screen.dart
+++ bitcoin-ticker-flutter/lib/price_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'coin_data.dart' as coinData;
+import 'package:flutter/cupertino.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +8,29 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
+  List<DropdownMenuItem<String>> getDropdownItems() {
+    List<DropdownMenuItem<String>> dropdownItems = [];
+    for (String currencyName in coinData.currenciesList) {
+      dropdownItems.add(
+        DropdownMenuItem<String>(
+          child: Text(currencyName),
+          value: currencyName,
+        ),
+      );
+    }
+    return dropdownItems;
+  }
+
+  List<Text> getPickerItems() {
+    List<Text> pickerItems = [];
+    for (String currencyName in coinData.currenciesList) {
+      pickerItems.add(Text(currencyName));
+    }
+    return pickerItems;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,10 +67,27 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: CupertinoPicker(
+              backgroundColor: Colors.lightBlue,
+              itemExtent: 32.0,
+              onSelectedItemChanged: (selectedIndex) {
+                print(selectedIndex);
+              },
+              children: getPickerItems(),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+// DropdownButton<String>(
+//               value: selectedCurrency,
+//               items: getDropdownItems(),
+//               onChanged: (value) {
+//                 setState(() {
+//                   selectedCurrency = value;
+//                 });
+//               },
+//             )
