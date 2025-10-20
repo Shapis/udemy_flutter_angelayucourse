6d1a0091caee04537db2c4c17fa3fba01ce51adf
Henri da Silva <hpsilva@pm.me>
2025-10-20 19:15:38 -0300

    Move all files out of old and back to the root directory

--- i_am_poor/android/app/src/main/kotlin/dev/vespertine/i_am_poor/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.vespertine.i_am_poor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- i_am_rich/android/app/src/main/kotlin/dev/vespertine/i_am_rich/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package dev.vespertine.i_am_rich
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
